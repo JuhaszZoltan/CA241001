8606d28fa12d384601529ecf40bf71e223ad9854
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-10-01 10:27:29 +0200

    Add project files.

---
+++ Event.cs
@@ -0,0 +1,33 @@
+﻿namespace CA241001
+{
+    internal class Event
+    {
+        public int Year { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public Dictionary<string, int> Medals { get; set; }
+
+        public override string ToString()
+        {
+            return
+                $"\tÉv:         {Year}\n" +
+                $"\tOrszág:     {Country}\n" +
+                $"\tVáros:      {City}\n" +
+                $"\tEredmények: a:{Medals["gold"]} | e:{Medals["silver"]} | b:{Medals["bronze"]}\n";
+        }
+
+        public Event(string row)
+        {
+            var v = row.Split(";");
+            Year = int.Parse(v[0]);
+            Country = v[1];
+            City = v[2];
+            Medals = new()
+            {
+                { "gold",   int.Parse(v[3]) },
+                { "silver", int.Parse(v[4]) },
+                { "bronze", int.Parse(v[5]) },
+            };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using CA241001;
+using System.Text;
+
+List<Event> events = [];
+
+using StreamReader sr = new(
+    path: @"..\..\..\src\paralympics.txt",
+    encoding: Encoding.UTF8);
+_ = sr.ReadLine();
+while (!sr.EndOfStream) events.Add(new(sr.ReadLine()));
+
+Console.WriteLine("1. feladat: " +
+    $"a paralimpia eddig {events.Count} alkalommal kerult megrendezesre.");
+
+var f2 = events.Sum(e => e.Medals.Values.Sum());
+Console.WriteLine("2. feladat: " +
+    $"a magyar sportolok osszesen {f2} ermet szereztek");
+
+var f3 = events.Where(e => e.Medals.Values.All(x => x == 0));
+Console.WriteLine("3. feladat: a kov helyszineken nem kepviseltette magat Mo:");
+foreach (var e in f3) Console.WriteLine($"\t{e.City} ({e.Year})");
+
+var f4 = events.GroupBy(e => e.Country).Where(g => g.Count() > 1);
+Console.WriteLine("4. feladat: azon orszagok, akik tobb paralimpiat is rendeztek:");
+foreach (var g in f4) Console.WriteLine($"\t{g.Key} ({g.Count()} alkalom)");
+
+Console.Write("5. feladat: irjon be egy evszamot: ");
+var f5val = int.TryParse(Console.ReadLine(), out int year);
+if (!f5val) Console.WriteLine("\tnem megfelelo a beirt formatum");
+else
+{
+    var f5 = events.SingleOrDefault(e => e.Year == year);
+    if (f5 is null) Console.WriteLine("\tebben az evben rem rendeztek paralimpiai jatekokat");
+    else Console.WriteLine(f5);
+}
